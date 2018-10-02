221a9fc02e8f8040f9c2d21f9ebd1a5c3210ea6f
Carlos Allende <carlosallendesanz@gmail.com>
2018-10-01 17:34:32 -0700

    comment model created, datastorage receiving comments succesfully given postID

--- src/app/dashboard/posts-list/post/post.component.ts
+++ src/app/dashboard/posts-list/post/post.component.ts
@@ -1,4 +1,11 @@
+// angular imports
 import { Component, OnInit, Input } from '@angular/core';
+// services imports
+import { DataStorageService } from '../../../../services/data-storage.service';
+// models imports
+import { Post } from '../../../../models/post.model';
+import { Comment } from '../../../../models/comment.model';
+
 
 @Component({
   selector: 'app-post',
@@ -6,10 +13,14 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-  @Input() post;
-  constructor() { }
+  @Input() post: Post;
+  comments: Comment[];
+  constructor(private dataStorageService: DataStorageService) { }
 
   ngOnInit() {
+    this.dataStorageService.fetchComments(this.post.id).subscribe((data: Comment[]) => {
+      console.log(data);
+    });
   }
 
 }
--- src/services/data-storage.service.ts
+++ src/services/data-storage.service.ts
@@ -1,22 +1,25 @@
+// angular imports
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-
+// models imports
 import { User } from '../models/user.model';
 import { Post } from '../models/post.model';
+import { Comment } from '../models/comment.model';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   user: User;
   posts: Post[];
+  comments: Comment[];
 
   storeUser(user: User) {
     this.user = user;
   }
 
   fetchPosts(userId: number) {
-    const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
+    const postsUrl = "https://jsonplaceholder.typicode.com/posts";
     return this.http.get(postsUrl).pipe(
       map(data => {
         return this.processPostsResponse(data, userId);
@@ -29,4 +32,16 @@ export class DataStorageService {
     return this.posts;
   }
 
+  fetchComments(postId: number) {
+    const commentsUrl = "https://jsonplaceholder.typicode.com/comments";
+    return this.http.get(commentsUrl).pipe(
+      map(data => {
+        return this.processCommentsResponse(data, postId);
+      })
+    );
+  }
+
+  processCommentsResponse(data, id) {
+    return (this.comments = data.filter(comment => comment.postId === id));
+  }
 }
